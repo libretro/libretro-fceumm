1203585fa2025c55b57851b25b28babb898abae7
twinaphex <libretro@gmail.com>
2017-04-30 03:28:37 +0200

    Buildfix

--- src/boards/unrom512.c
+++ src/boards/unrom512.c
@@ -193,7 +193,8 @@ static void UNROM512LSync() {
 		}
 		else if (latche==0x10)
 		{
-			for(uint32 i=0;i<(ROM_size*4);i++)
+         uint32 i;
+			for(i=0;i<(ROM_size*4);i++)
 				inc_flash_write_count(i>>2,i<<12);
 			memset(flashdata,0xFF,ROM_size*0x4000);	//Erasing the rom chip as instructed. Crash rate calulated to be 99.9% :)
 		}
