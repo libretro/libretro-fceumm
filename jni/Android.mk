5f08fd57f93c3413d9f4582b119b56e6217bb4bb
Aaron Kling <webgeek1234@gmail.com>
2018-04-06 13:22:38 -0500

    Refactor android jni makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,34 +1,19 @@
 LOCAL_PATH := $(call my-dir)
-GIT_VERSION := " $(shell git rev-parse --short HEAD)"
+
+CORE_DIR := $(LOCAL_PATH)/../src
+
+include $(LOCAL_PATH)/../Makefile.common
+
+COREFLAGS := $(COREDEFINES) -DPSS_STYLE=1 $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+	COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
 
 include $(CLEAR_VARS)
-
-LOCAL_CFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-CORE_DIR := ../src
-
 LOCAL_MODULE    := retro
-
-include ../Makefile.common
-
-LOCAL_SRC_FILES  = $(SOURCES_C)
-
-LOCAL_CFLAGS += -DWANT_GRIFFIN $(COREDEFINES) -DPSS_STYLE=1
-LOCAL_C_INCLUDES = $(LOCAL_PATH)/$(CORE_DIR)
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(CORE_DIR)/drivers/libretro \
-						  $(LOCAL_PATH)/$(CORE_DIR)/drivers/libretro/libretro-common/include
-
+LOCAL_SRC_FILES := $(SOURCES_C)
+LOCAL_CFLAGS    := $(COREFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/drivers/libretro/link.T
 include $(BUILD_SHARED_LIBRARY)
