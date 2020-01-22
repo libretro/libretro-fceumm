583a44dfd84e78b0689812391011b7ba75be8253
negativeExponent <negativeExponent@users.noreply.github.com>
2020-01-23 07:49:08 +0800

    Update mapper 213

--- src/boards/addrlatch.c
+++ src/boards/addrlatch.c
@@ -345,8 +345,14 @@ void Mapper212_Init(CartInfo *info) {
 /*------------------ Map 213 ---------------------------*/
 
 static void M213Sync(void) {
+	if(latche & 0x40) {
+		setprg16(0x8000, (latche & 7));
+		setprg16(0xC000, (latche & 7));
+	} else {
 	setprg32(0x8000, (latche >> 1) & 3);
+	}
 	setchr8((latche >> 3) & 7);
+	setmirror(((latche & 1)^((latche >> 6) & 1)) ^ 1);
 }
 
 void Mapper213_Init(CartInfo *info) {
