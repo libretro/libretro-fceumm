5538bbedd9455380bf04c56223775c6bc6f414d1
negativeExponent <negativeExponent@users.noreply.github.com>
2020-10-22 05:32:16 +0800

    Update mapper 059/060
    Excerpt from nesdev:
    
    NESten's Mapper DLL v1.0 contained an incorrect implementation of T3H53 as iNES mapper 059 and UNIF mapper BMC-T3H53. The UNIF implementation was improved in NESten's Mapper DLL v1.2 while the iNES implementation was not, causing confusion about what kind of cartridge the unimproved mapper 59 was supposed to be. As Nintendulator's mapper DLL is based on NESten's mapper DLL, this was carried over to Nintendulator. Presumably as a result of this confusion, FCEUX and others put the correct implementation of T3H53 at INES Mapper 060, displacing the "Reset-based NROM-128 4-in-1" in the process.
    
    With that said above, im following nesdev's notes and change the
    following:
    
    - Reassign Mapper 60 as Mapper 59.
    - Add BMC-T3H53, a UNIF cart using the same UNIF-board D1038 which is
      assigned as iNES Mapper 59
    - Add the Reset-Based NRON-128 mapper, Mapper 60

--- src/ines.c
+++ src/ines.c
@@ -479,8 +479,9 @@ INES_BOARD_BEGIN()
 	INES_BOARD( "UNLKS202",                  56, UNLKS202_Init          )
 	INES_BOARD( "SIMBPLE BMC PIRATE A",      57, Mapper57_Init          )
 	INES_BOARD( "SIMBPLE BMC PIRATE B",      58, BMCGK192_Init          )
-	INES_BOARD( "",                          59, Mapper59_Init          ) /* Check this out */
-	INES_BOARD( "SIMBPLE BMC PIRATE C",      60, BMCD1038_Init          )
+/*	INES_BOARD( "",                          59, Mapper59_Init         ) */ /* Check this out, update 2020-10-21 - formerly an incorrect implementation of T3H53 */
+	INES_BOARD( "BMC T3H53/D1038",           59, BMCD1038_Init          )
+	INES_BOARD( "Reset-based NROM-128 ",     60, Mapper60_Init          )
 	INES_BOARD( "20-in-1 KAISER Rev. A",     61, Mapper61_Init          )
 	INES_BOARD( "700-in-1",                  62, Mapper62_Init          )
 	INES_BOARD( "",                          63, Mapper63_Init          )
