7abe2fb5cfa4382b8914420f01f9e5be974dce27
tim942 <75698946+tim942@users.noreply.github.com>
2021-04-28 01:21:45 -0500

    Update libretro.c

--- src/drivers/libretro/libretro.c
+++ src/drivers/libretro/libretro.c
@@ -961,11 +961,11 @@ void retro_get_system_info(struct retro_system_info *info)
 static float get_aspect_ratio(unsigned width, unsigned height)
 {
   if (aspect_ratio_par == 1) {
-    return NES_8_7_PAR;
+    get_aspect_ratio = NES_8_7_PAR;
   } else if (aspect_ratio_par == 2) {
-    return NES_4_3;
+    get_aspect_ratio = NES_4_3;
   } else if (aspect_ratio_par == 3) {
-    return NES_PP;
+    get_aspect_ratio = NES_PP;
   }
 }
 
