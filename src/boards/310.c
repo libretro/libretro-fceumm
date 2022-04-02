a3f2054aa6b6981e91209ff4dcd84fa51a9f3a4e
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2022-04-02 17:49:05 +0200

    Correct 310.c copyright notice

--- src/boards/310.c
+++ src/boards/310.c
@@ -1,7 +1,7 @@
 /* FCE Ultra - NES/Famicom Emulator
  *
  * Copyright notice for this file:
- *  Copyright (C) 2005 CaH4e3
+ *  Copyright (C) 2022 NewRisingSun
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
