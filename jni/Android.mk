f9d99fc5d0951e576b926ff43a25d7b2e0c210a4
libretroadmin <reallibretroretroarch@gmail.com>
2024-01-24 16:37:49 +0100

    Move libretro dir and use LINK_T define

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 CORE_DIR := $(LOCAL_PATH)/../src
+LINK_T := $(LOCAL_PATH)/../libretro/link.T
 
 HAVE_NTSC := 1
 
@@ -17,5 +18,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := retro
 LOCAL_SRC_FILES := $(SOURCES_C)
 LOCAL_CFLAGS    := $(COREFLAGS)
-LOCAL_LDFLAGS   := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
+LOCAL_LDFLAGS   := -Wl,-version-script=$(LINK_T)
 include $(BUILD_SHARED_LIBRARY)
