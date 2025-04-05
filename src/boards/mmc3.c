181c525f9803544dc077da03ba199a1ee6efa6f5
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-04-05 22:25:49 +0200

    Added mapper 472.

--- src/boards/mmc3.c
+++ src/boards/mmc3.c
@@ -21,7 +21,7 @@
  */
 
 /*  Code for emulating iNES mappers 4,12,44,45,47,49,52,74,114,115,116,118,
- 119,165,205,245,249,250,254,361,366,367,373,392,555
+ 119,165,205,245,249,250,254,361,366,367,373,392,472,555
 */
 
 #include "mapinc.h"
@@ -1470,6 +1470,49 @@ void Mapper254_Init(CartInfo *info) {
 	info->Power = M254_Power;
 	AddExState(EXPREGS, 2, 0, "EXPR");
 }
+
+/* ---------------------------- Mapper 472 ------------------------------ */
+static void M472PW(uint32 A, uint8 V) {
+	setprg8(A, V &0x0F | EXPREGS[0] &0xF0);
+}
+
+static void M472CW(uint32 A, uint8 V) {
+	int chrAND =EXPREGS[0] &0x20? 0x7F: 0xFF;
+	setchr1(A, V &chrAND | EXPREGS[0] <<3 &~chrAND);
+}
+
+static DECLFR(M472Read) {
+	return EXPREGS[0];
+}
+
+static DECLFW(M472Write) {
+	EXPREGS[0] = EXPREGS[0] &0x80 | V &0x7F;
+	FixMMC3PRG(MMC3_cmd);
+	FixMMC3CHR(MMC3_cmd);
+}
+
+static void M472Reset(void) {
+	EXPREGS[0] =(EXPREGS[0] ^0x80) &0x80;
+	FixMMC3PRG(MMC3_cmd);
+	FixMMC3CHR(MMC3_cmd);
+}
+
+static void M472Power(void) {
+	EXPREGS[0] = 0;	
+	GenMMC3Power();
+	SetReadHandler(0x6000, 0x7FFF, M472Read);
+	SetWriteHandler(0x6000, 0x7FFF, M472Write);
+}
+
+void Mapper472_Init(CartInfo *info) {
+	GenMMC3_Init(info, 128, 256, 8, 0);
+	pwrap = M472PW;
+	cwrap = M472CW;
+	info->Reset = M472Reset;
+	info->Power = M472Power;
+	AddExState(EXPREGS, 1, 0, "EXPR");
+}
+
 
 /* ---------------------------- Mapper 555 ------------------------------ */
 
