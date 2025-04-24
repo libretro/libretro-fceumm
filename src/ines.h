2d0170471a0c7bc5eeb5e3d1388491351acfb725
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-04-25 01:38:35 +0200

    Add mapper 475.

--- src/ines.h
+++ src/ines.h
@@ -375,6 +375,7 @@ void Mapper471_Init(CartInfo *);
 void Mapper472_Init(CartInfo *);
 void Mapper473_Init(CartInfo *);
 void Mapper474_Init(CartInfo *);
+void Mapper475_Init(CartInfo *);
 void Mapper481_Init(CartInfo *);
 void Mapper484_Init(CartInfo *);
 void Mapper487_Init(CartInfo *);
