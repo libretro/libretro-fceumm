9ca7e0254f67448999fbaddd3b9811dbb70bfc4b
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-04-24 23:43:08 +0200

    Add mapper 406, together with generic self-flashing code.

--- src/ines.h
+++ src/ines.h
@@ -319,6 +319,7 @@ void Mapper399_Init(CartInfo *);
 void Mapper401_Init(CartInfo *);
 void Mapper403_Init(CartInfo *);
 void Mapper404_Init(CartInfo *);
+void Mapper406_Init(CartInfo *);
 void Mapper409_Init(CartInfo *);
 void Mapper410_Init(CartInfo *);
 void Mapper411_Init(CartInfo *);
