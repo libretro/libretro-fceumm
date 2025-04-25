802bb7d11a36df1ddbe2451b059c0ee83ddc8617
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-04-25 20:39:44 +0200

    Added mapper 478 (based on a single cartridge, so subject to change).

--- src/ines.h
+++ src/ines.h
@@ -378,6 +378,7 @@ void Mapper474_Init(CartInfo *);
 void Mapper475_Init(CartInfo *);
 void Mapper476_Init(CartInfo *);
 void Mapper477_Init(CartInfo *);
+void Mapper478_Init(CartInfo *);
 void Mapper481_Init(CartInfo *);
 void Mapper484_Init(CartInfo *);
 void Mapper487_Init(CartInfo *);
