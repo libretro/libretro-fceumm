edd3029dc4d5fb2fc3ed4d123fd28a53ad23152f
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-09-09 11:26:45 +0200

    Add mappers 506 and 572.

---
+++ src/boards/572.c
@@ -0,0 +1,60 @@
+/* FCEUmm - NES/Famicom Emulator
+ *
+ * Copyright notice for this file:
+ *  Copyright (C) 2020
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ */
+
+#include "mapinc.h"
+#include "asic_mmc3.h"
+
+static uint8 reg;
+
+static void sync() {
+	int prgAND = 0x1F;
+	int chrAND = 0x7F;
+	int prgOR  = reg <<5;
+	int chrOR  = reg <<7;
+	MMC3_syncPRG(prgAND, prgOR &~prgAND);
+	MMC3_syncCHR(chrAND, chrOR &~chrAND);
+	MMC3_syncMirror();
+}
+
+static DECLFW(writeReg) {
+	if (~reg &0x08) {
+		reg = A &0xFF;
+		sync();
+	}
+}
+
+static void reset() {
+	reg = 0;
+	sync();
+}
+
+static void power() {
+	reg = 0;
+	MMC3_power();
+}
+
+void Mapper572_Init(CartInfo *info) {
+	MMC3_init(info, sync, MMC3_TYPE_AX5202P, NULL, NULL, NULL, writeReg);
+	info->Power = power;
+	info->Reset = reset;
+	AddExState(&reg, 1, 0, "EXPR");
+}
