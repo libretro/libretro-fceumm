e4cf15709c515049f45a4ae83c26d237c4b66cea
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2025-09-09 21:31:39 +0200

    Add mapper 511.

--- src/ines.h
+++ src/ines.h
@@ -395,6 +395,7 @@ void Mapper502_Init(CartInfo *);
 void Mapper503_Init(CartInfo *);
 void Mapper504_Init(CartInfo *);
 void Mapper506_Init(CartInfo *);
+void Mapper511_Init(CartInfo *);
 void Mapper516_Init(CartInfo *);
 void Mapper520_Init(CartInfo *);
 void Mapper523_Init(CartInfo *);
