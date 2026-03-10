eb6df7680aceb8212d9b4f3e38574c92800f0a62
NewRisingSun <8vytz1+dhp372pv94ebg@sharklasers.com>
2026-03-10 19:25:49 +0100

    Add mapper 612.

---
+++ src/boards/612.c
@@ -0,0 +1,47 @@
+/* FCE Ultra - NES/Famicom Emulator
+ *
+ * Copyright notice for this file:
+ *  Copyright (C) 2026 NewRisingSun
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "mapinc.h"
+#include "asic_latch.h"
+
+static uint8 reg;
+
+static void sync () {
+	setprg16(0x8000, reg <<3 | Latch_data &0x07);
+	setprg16(0xC000, reg <<3 |             0x07);
+	setchr8(0);
+}
+
+static void reset () {
+	reg++;
+	Latch_clear();
+}
+
+static void power () {
+	reg = 0;
+	Latch_power();
+}
+
+void Mapper612_Init (CartInfo *info) {
+	Latch_init(info, sync, 0x8000, 0xFFFF, NULL);
+	info->Power = power;
+	info->Reset = reset;
+	AddExState(&reg, 1, 0, "REGS");
+}
